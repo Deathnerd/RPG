dac8530d7e4dcd106095468e614e7a7e13140f60
Wes Gilleland <wes.gilleland@gmail.com>
2015-04-10 13:17:36 -0400

    Changed to MySQL for ease of development

--- rest_rpg/settings.py
+++ rest_rpg/settings.py
@@ -7,7 +7,7 @@ class Base():
 	SECRET_KEY = "S00pp3rS3cr3t"
 	APP_DIR = os.path.abspath(os.path.dirname(__file__))
 	PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
-	SQLALCHEMY_DATABASE_URI = "sqlite:///dbase.db"
+	SQLALCHEMY_DATABASE_URI = "mysql://rpg:rpg@localhost/rpg"
 	BCRYPT_LOG_ROUNDS = 13
 	DEBUG_TB_ENABLED = False
 	DEBUG_TB_INTERCEPT_REDIRECTS = False
